9de78288aa18b6cdbca72f20f41909b60518cf05
Esha kaprvwan <eshakaparvwan@gmail.com>
2021-04-07 02:03:01 +0530

    modal added

--- ecom/shop/models.py
+++ ecom/shop/models.py
@@ -12,3 +12,14 @@ class Product(models.Model):
     image=models.ImageField(upload_to='shop/media/images', default="")
     def __str__(self):
         return self.product_name + " "+ self.category
+
+
+class Contact(models.Model):
+    msg_id = models.AutoField
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=70, default="")
+    phone = models.CharField(max_length=70, default="")
+    desc = models.CharField(max_length=500, default="")
+    
+    def __str__(self):
+        return self.name
--- ecom/shop/views.py
+++ ecom/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Contact
 # Create your views here.
 from math import ceil
 def index(request):
@@ -25,6 +25,17 @@ def index(request):
 def about(request):
     return render(request,'shop/about.html')
 def contact(request):
+    if request.method=="POST" :
+        
+        name=request.POST.get('name','')
+        #print(name)
+        email=request.POST.get('email','')
+        phone=request.POST.get('phone','')
+        desc=request.POST.get('desc','')
+        # print(name,email,phone,email)
+        contact=Contact(name=name,email=email,phone=phone,desc=desc)
+        contact.save()
+    
     return render(request,'shop/contact.html')
 def tracker(request):
     return render(request,'shop/tracker.html')
